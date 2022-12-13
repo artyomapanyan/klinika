1feef508c2702b6754cfa74def006c0d303cf710
Hayk <gzoghyanh@gmail.com>
2022-12-13 13:24:30 +0400

    Fixes

--- src/Components/Fragments/ResourceSelectPaginated.js
+++ src/Components/Fragments/ResourceSelectPaginated.js
@@ -4,7 +4,7 @@ import {useGetResourceIndex} from "../Functions/api_calls";
 
 function ResourceSelectPaginated({initialData = [], resource}) {
     const timeout = useRef(null);
-    const [params, setParams] = useState({page:1})
+    const [params, setParams] = useState({page: 1})
     const [localData, setLocalData] = useState(initialData)
 
     const {loadingState, dataState} = useGetResourceIndex(resource, params)
@@ -15,42 +15,42 @@ function ResourceSelectPaginated({initialData = [], resource}) {
             return <Select.Option key={item.id} value={item.id} name={item.name}>{item.name}</Select.Option>
         })
     }
-    useEffect(()=>{
-        setLocalData([...localData,...(data?.items??[])])
-    },[data])
+    useEffect(() => {
+        setLocalData([...localData, ...(data?.items ?? [])])
+    }, [data])
 
     const handleScroll = (event) => {
         let target = event.target
         if (target.scrollTop + target.offsetHeight === (target.scrollHeight)) {
-            if(timeout.current){
+            if (timeout.current) {
                 clearTimeout(timeout.current)
             }
-            if(data.pagination.last_page>=params.page+1){
-                timeout.current = setTimeout(()=>{
+            if (data.pagination.last_page >= params.page + 1) {
+                timeout.current = setTimeout(() => {
                     setParams({
-                        ...params,
-                        page:params.page+1
+                        ...params, page: params.page + 1
                     })
 
-                },300)
+                }, 300)
             }
         }
     }
-    const handleSearch = (e)=>{
-        if(timeout.current){
+    const handleSearch = (e) => {
+        if (timeout.current) {
             clearTimeout(timeout.current)
         }
-        timeout.current = setTimeout(()=>{
+        timeout.current = setTimeout(() => {
             setLocalData([])
             setParams({
-                page:1,
-                name:e
+                page: 1, name: e
             })
-        },500)
+        }, 500)
     }
-    return <Select loading={loading} onPopupScroll={handleScroll} onSearch={handleSearch} showSearch optionFilterProp={'name'}>
+    return <Select loading={loading} onPopupScroll={handleScroll} onSearch={handleSearch} showSearch
+                   optionFilterProp={'name'}>
         {handleGenerateOptions(localData ?? [])}
-        {loading ? <Select.Option value={999} style={{textAlign: 'center'}} name={params.name}><Spin/></Select.Option> : null}
+        {loading ?
+            <Select.Option value={999} style={{textAlign: 'center'}} name={params.name}><Spin/></Select.Option> : null}
 
 
     </Select>
