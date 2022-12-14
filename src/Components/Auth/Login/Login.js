30b29a967acc184f4427b5d4985bbe91a57b2bad
Hayk <gzoghyanh@gmail.com>
2022-12-14 19:38:37 +0400

    Fixes

--- src/Components/Auth/Login/Login.js
+++ src/Components/Auth/Login/Login.js
@@ -1,4 +1,4 @@
-import {Button, Carousel, Checkbox, Form, Input, Select, Spin, Tabs} from "antd";
+import {Button, Carousel, Checkbox, Form, Input, Space} from "antd";
 import React, {useRef, useState} from 'react';
 import "./Login.sass";
 import logo from "../../../dist/Img/logo.svg";
@@ -6,10 +6,7 @@ import {useDispatch} from "react-redux";
 import {useNavigate} from "react-router";
 import axios from "axios";
 import api from "../../../Api";
-import {EyeInvisibleOutlined} from "@ant-design/icons";
 import {Link} from "react-router-dom";
-
-
 
 
 function Login() {
@@ -19,37 +16,45 @@ function Login() {
     const navigate = useNavigate();
 
     const [rolesState, setRolesState] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const handleLoginWithRole = (role)=>{
+    const [login, setLogin] = useState({});
+    const [loading, setLoading] = useState({});
+    const handleLoginWithRole = (role) => {
         handleLogin({
-            ...formRef.current.getFieldsValues(),
-            role_id:role.id
+            ...login,
+            role_id: role.id
         })
     }
-    const handleLogin = (values)=>{
-
+    const handleLogout = () => {
+        formRef.current.resetFields()
+        carouselRef.current.prev()
+    }
+    const handleLogin = (values) => {
+        setLoading({
+            [values.role_id ?? 'save']: true
+        })
         axios.get(`${api.endpoint}/sanctum/csrf-cookie`).then(() => {
-            setLoading(true)
+
             values.device_name = 'React App'
             axios.request({
                 url: api.Auth.login.url,
                 method: api.Auth.login.method,
                 data: values,
-            }).then(response=>{
-                if(response.user.id){
-                    if(!response?.token){
+            }).then(response => {
+                if (response.user.id) {
+                    if (!response?.token) {
                         setRolesState(response?.user?.roles)
+                        setLogin(values)
                         carouselRef.current.next()
-                    }else{
+                    } else {
                         dispatch({
-                            type:'AUTH',
-                            payload:response
+                            type: 'AUTH',
+                            payload: response
                         })
                         navigate('/dashboard')
                     }
                 }
-            }).finally(()=>{
-                setLoading(false)
+            }).finally(() => {
+                setLoading({})
             })
         })
 
@@ -57,67 +62,56 @@ function Login() {
 
     return (
         <div className={'login_background'}>
-                <div className={'card_div'}>
+            <div className={'card_div'}>
 
-                    <Carousel dots={false} ref={carouselRef}>
-                                <div className={'logo_div'}>
-                                <img src={logo} alt={'logo'} />
-                                <div className={'form_div'}>
-                                    <Form
-                                        ref={formRef}
-                                        onFinish={handleLogin}>
-                                        <Form.Item
-                                            name={'email'}
-                                        >
+                <Carousel dots={false} ref={carouselRef}>
+                    <div className={'logo_div'}>
+                        <img src={logo} alt={'logo'}/>
+                        <div className={'form_div'}>
+                            <Form
+                                ref={formRef}
+                                onFinish={handleLogin}>
+                                <Form.Item
+                                    name={'email'}
+                                >
 
-                                            <Input placeholder="Email" />
-                                        </Form.Item>
+                                    <Input placeholder="Email"/>
+                                </Form.Item>
 
-                                        <Form.Item
-                                            name={'password'}
-                                        >
-                                            <Input.Password
-                                                iconRender={() => (<Link onClick={(e)=>{
-                                                    e.preventDefault();
-                                                    e.stopPropagation()
-                                                }}
-                                                                         style={{position:'absolute', marginLeft: -60}} to="/forgot" >Forgot?</Link>)}
-                                                placeholder="Password"
-                                                style={{position:'static'}}
-                                            />
-                                        </Form.Item>
-
-
-
-                                        {rolesState.length?<Form.Item
-                                            name={'role_id'}>
-                                            <Select>
-                                                {rolesState.map((el) => {
-                                                    return <Select.Option key={el.id} value={el.id}>{el.name}</Select.Option>
-                                                })}
-                                            </Select>
-                                        </Form.Item>:null}
-                                        <div className={'log_check_div'}>
-                                            <Button className={'login_btn'} loading={loading??<Spin />} type={'primary'} htmlType={'submit'}>Login</Button>
-                                            <Checkbox>Remember me</Checkbox>
-                                        </div>
-
-
-                                    </Form>
+                                <Form.Item
+                                    name={'password'}
+                                >
+                                    <Input.Password
+                                        iconRender={() => (<Link onClick={(e) => {
+                                            e.preventDefault();
+                                            e.stopPropagation()
+                                        }}
+                                                                 style={{position: 'absolute', marginLeft: -60}}
+                                                                 to="/forgot">Forgot?</Link>)}
+                                        placeholder="Password"
+                                        style={{position: 'static'}}
+                                    />
+                                </Form.Item>
+                                <div className={'log_check_div'}>
+                                    <Button className={'login_btn'} loading={loading.save} type={'primary'}
+                                            htmlType={'submit'}>Login</Button>
+                                    <Checkbox>Remember me</Checkbox>
                                 </div>
-                                </div>
-                                 <div className={'logo_div'}>
-                                     {rolesState.map(role=><Button key={role.id} type={'primary'} onClick={()=>handleLoginWithRole(role)} loading={loading}>{role.name}</Button>)}
+                            </Form>
+                        </div>
+                    </div>
+                    <div className={'logo_div'}>
+                        <Space direction={'vertical'}>
+                            {rolesState.map(role => <Button key={role.id} type={'primary'}
+                                                            onClick={() => handleLoginWithRole(role)}
+                                                            loading={loading[role.id]}>{role.name}</Button>)}
+                            <Button type={'secondary'} onClick={handleLogout}>Log out</Button>
+                        </Space>
+                    </div>
+                </Carousel>
 
-                                 </div>
-                    </Carousel>
 
-
-
-
-
-
-                </div>
+            </div>
         </div>
     )
 }
