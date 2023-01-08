a2e80e390046f4b1830bce880d08fb1852affaa5
Hayk <gzoghyanh@gmail.com>
2023-01-08 16:46:40 +0400

    Chart fixes

--- src/Components/Fragments/Charts/GradientChart.js
+++ src/Components/Fragments/Charts/GradientChart.js
@@ -4,44 +4,28 @@ import {Dropdown, Space} from "antd";
 import {DownOutlined} from "@ant-design/icons";
 import GradientChartApp from "../../Dashboard/ClinicsOwner/Fragments/GradientChartApp";
 
-function GradientChart() {
+function GradientChart({data}) {
     let canvasRef = useRef();
     let appointmentChartRef = useRef(null)
 
-    const previousData = [
-        0, 54, 130, 100, 220, 122, 380, 220, 355, 117, 352, 40,
-    ];
-    const approvedData = [
-        24, 114, 200, -10, 120, 250, 513, 117, 209, 520, 120, 110,
-    ];
-    let maxValue = Math.max(...previousData);
-    let maxValueIndex = previousData.indexOf(maxValue);
-    const canceledData = [10, 30, 20, -80, 20, 30, 50, 80, 50, 20, 10, 5];
 
 
-    const verticalLine = {
-        id: "verticalLine",
-        beforeDraw(chart, args, opions) {
-            const {
-                ctx,
-                chartArea: { top, right, bottom, left, width, height },
-                scales: { x, y },
-            } = chart;
-            ctx.save();
-            ctx.setLineDash([2, 2]);
-            ctx.strokeStyle = "#635D6B";
-            ctx.strokeRect(
-                x.getPixelForValue(maxValueIndex),
-                top - 20,
-                0,
-                height + 20
-            );
-            ctx.restore();
-        },
-    };
 
 
+
+        useEffect(()=>{
+
+        },[data])
     useEffect(()=>{
+        const previousData = [
+            0, 54, 130, 100, 220, 122, 380, 220, 355, 117, 352, 40,
+        ];
+        const approvedData = [
+            24, 114, 200, -10, 120, 250, 513, 117, 209, 520, 120, 110,
+        ];
+        let maxValue = Math.max(...previousData);
+        let maxValueIndex = previousData.indexOf(maxValue);
+        const canceledData = [10, 30, 20, -80, 20, 30, 50, 80, 50, 20, 10, 5];
         const appointmentsStats = canvasRef.current.getContext("2d")
         Chart.register(...registerables)
 
@@ -51,7 +35,26 @@ function GradientChart() {
         gradient.addColorStop(1, "rgba(191, 83, 158, 1)");
         gradientGrey.addColorStop(0, "rgba(217, 217, 217, 0)");
         gradientGrey.addColorStop(1, "rgba(217, 217, 217, 1)");
-
+        const verticalLine = {
+            id: "verticalLine",
+            beforeDraw(chart) {
+                const {
+                    ctx,
+                    chartArea: { top,height },
+                    scales: { x },
+                } = chart;
+                ctx.save();
+                ctx.setLineDash([2, 2]);
+                ctx.strokeStyle = "#635D6B";
+                ctx.strokeRect(
+                    x.getPixelForValue(maxValueIndex),
+                    top - 20,
+                    0,
+                    height + 20
+                );
+                ctx.restore();
+            },
+        };
         appointmentChartRef.current = new Chart(appointmentsStats, {
         type: "line",
         data: {
@@ -82,15 +85,11 @@ function GradientChart() {
                     pointBorderColor: "#C1BEC4",
                     pointStyle: "circle",
                     pointBackgroundColor: "#C1BEC4",
-                    pointRadius: function (context) {
-                        let value = previousData[context.dataIndex];
-                        return maxValue === value ? 4 : 0;
-                    },
+                    pointRadius: 1,
                 },
                 {
                     label: "Approved",
                     data: approvedData,
-                    // stack: 'Stack 0',
                     backgroundColor: gradient,
                     fill: "start",
                     borderColor: ["rgba(191, 83, 158, 1)"],
@@ -100,11 +99,6 @@ function GradientChart() {
                     pointBorderColor: "white",
                     pointStyle: "circle",
                     pointRadius: 5,
-                    // pointBackgroundColor:"#6DAF56",
-                    pointBackgroundColor: function (context) {
-                        let value = approvedData[context.dataIndex];
-                        return value > 0 ? "#6DAF56" : "#CF533E";
-                    },
                 },
                 {
                     label: "Previous",
@@ -115,16 +109,12 @@ function GradientChart() {
                     borderWidth: 2,
                     borderRadius: 222,
                     type: "line",
-                    pointRadius: function (context) {
-                        let value = previousData[context.dataIndex];
-                        return maxValue === value ? 4 : 0;
-                    },
                     pointBorderColor: "#774D9D",
                     pointBackgroundColor: "#774D9D",
+                    pointRadius: 3,
                 },
             ],
         },
-
         options: {
             responsive: true,
             maintainAspectRatio: false,
@@ -155,7 +145,7 @@ function GradientChart() {
                 x: {
                     beginAtZero: false,
                     grid: {
-                        drawBorde: false,
+                        drawBorder: false,
                         display: false,
                         borderColor: "white",
                     },
