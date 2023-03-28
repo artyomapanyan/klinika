e0ac0b9ae103f069c72126c8357b925029360056
Tigran Ghumashyan <tigranghumashyan@gmail.com>
2023-03-28 17:56:44 +0400

    fixes

--- src/Components/Dashboard/Clinics/Fragments/ShowClinic/ShowClinicDoctorsTab.jsx
+++ src/Components/Dashboard/Clinics/Fragments/ShowClinic/ShowClinicDoctorsTab.jsx
@@ -3,28 +3,30 @@ import { Avatar, Card, Typography } from 'antd';
 const ShowClinicDoctorsTab = ({ desc, companies }) => {
   const { Paragraph, Text } = Typography;
   const doctor = {
-    name: "Lorem Ipsum",
-    image: "https://randomuser.me/api/portraits/men/31.jpg",
+    name: 'Lorem Ipsum',
+    image: 'https://randomuser.me/api/portraits/men/31.jpg',
     doctorSpecialities: [
-      "Psychiatry",
-      "Neurosurgery",
-      "Vascular Surgery"
-    ]
-  }
+      'Psychiatry',
+      'Neurosurgery',
+      'Vascular Surgery',
+    ],
+  };
   return (
-    <Card style={{ width: "100%", marginTop: 16 }}>
-      <Card.Meta
-        avatar={<Avatar src={doctor.image}/>}
-        title={doctor.name}
-        description={
-          <Paragraph>
-            {doctor?.doctorSpecialities?.map ((speciality, i) => {
-              return <Text code key={i}>{speciality}</Text>;
-            })}
-          </Paragraph>
-        }
-      />
-    </Card>
+    <div className="add_edit_content">
+      <Card style={{ width: '100%', marginTop: 16 }}>
+        <Card.Meta
+          avatar={<Avatar src={doctor.image}/>}
+          title={doctor.name}
+          description={
+            <Paragraph>
+              {doctor?.doctorSpecialities?.map ((speciality, i) => {
+                return <Text code key={i}>{speciality}</Text>;
+              })}
+            </Paragraph>
+          }
+        />
+      </Card>
+    </div>
   );
 };
 export default ShowClinicDoctorsTab;
