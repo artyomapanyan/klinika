8ec7c0c787e083fcdade7988ee009983c42b52ef
hayk <gzoghyanh@gmail.com>
2023-04-28 16:20:56 +0400

    Create Clinic uploads

--- src/Components/Dashboard/DoctorReworked/Fragments/DoctorReworkedCalendar/DoctorReworkedCalendar.js
+++ src/Components/Dashboard/DoctorReworked/Fragments/DoctorReworkedCalendar/DoctorReworkedCalendar.js
@@ -7,123 +7,87 @@ import {postResource} from "../../../../Functions/api_calls";
 import {useSelector} from "react-redux";
 import dayjs from "dayjs";
 import Preloader from "../../../../Preloader";
-
+import {Spin} from "antd";
 
 
 function DoctorReworkedCalendar() {
     let token = useSelector((state) => state.auth.token);
 
     const [date, setDate] = useState({
-        from: dayjs().add(-12, 'month').format('YYYY-MM-DD'),
-        to: dayjs().format('YYYY-MM-DD'),
+        from: dayjs().add(-3, 'day').format('YYYY-MM-DD'), to: dayjs().format('YYYY-MM-DD'),
     })
+    const [view, setView] = useState('3 Days')
 
     const [loading, setLoading] = useState(false);
     const [appointments, setAppointments] = useState([]);
 
 
-
-
-
     useEffect(() => {
         setLoading(true)
         postResource('DoctorReworked', 'DoctorCalendar', token, '', date).then((response) => {
-            console.log(response,'res')
-            let data = Object.values(response.calendar).flat().map(e=>({
-                text: 'Website Re-Design Planfffffffffffffffffff',
+            let data = Object.values(response.calendar).flat().map(e => ({
+                text: e.service_type,
                 startDate: new Date(e.booked_at.iso_string),
                 endDate: new Date(e.booked_to.iso_string),
-                roomId: [1],
-
-                ...e
+                content:e.service_name,
+                roomId: [1], ...e
             }));
             setAppointments(data)
             setLoading(false)
         })
-    }, [])
+    }, [date])
 
 
-
-  //   const a = [
-  //    {
-  //      text: 'Website Re-Design Plan',
-  //      startDate: new Date('2022-06-22T12:30:00+04:00'),
-  //      endDate: new Date('2022-06-22T13:30:00+04:00'),
-  //       roomId: [1],
-  //   },
-  // ];
-
-
-    const resourcesData = [
-        {
-            text: 'Room 101',
-            id: 1,
-            color: '#43285d',
-        }, {
-            text: 'Room 102',
-            id: 2,
-            color: '#6DAF56',
-        }, {
-            text: 'Room 103',
-            id: 3,
-            color: '#BF539E',
-        }, {
-            text: 'Meeting room',
-            id: 4,
-            color: '#FFBD0D',
-        }, {
-            text: 'Conference hall',
-            id: 5,
-            color: '#635D6B',
-        },
-    ];
-
-
-
-
-    return(
-        <div className={'dr_reworked_not'}>
-            <DoctorReworkedCalendarHeader />
+    return (<div className={'dr_reworked_not'}>
+            <DoctorReworkedCalendarHeader/>
 
             <div className={'dr_reworked_calendar_div'}>
-                {
-                    loading ? <Preloader /> : <Scheduler
-                        dataSource={appointments}
-                        height={500}
-                        defaultCurrentDate={new Date()}
+                <Spin spinning={loading}> <Scheduler
+                    dataSource={appointments}
+                    height={500}
+                    currentView={view}
+                    onCurrentDateChange={(e) => {
+                        setDate(prevState => {
+                            const currentDate = dayjs(e);
+                            return {
+                                from: currentDate.add(-10, 'day').format('YYYY-MM-DD'),
+                                to: currentDate.add(10, 'day').format('YYYY-MM-DD')
+                            }
+                        })
+                    }}
+                    onCurrentViewChange={(e) => {
+                        setView(e)
+                    }}
+                    defaultCurrentDate={date.from}
 
-                        appointmentRender={(e)=> <CalendarDataCell data={e}/>}
-                        views={
-                            [{
-                                type: 'day',
-                                name: '3 Days',
-                                intervalCount: 3,
-                            }, 'week']
-                        }
-                        editing={{
-                            allowAdding: false,
-                            allowDeleting: false,
-                            allowUpdating: false,
-                            allowResizing: false,
-                            allowDragging: false,
-                            allowTimeZoneEditing: false,
-                        }}
-                        onAppointmentClick={(e)=> e.cancel = true}
-                        onAppointmentDblClick={(e)=> e.cancel = true}
-                        onCellClick={(e)=> e.cancel = true}
-                        onAppointmentTooltipShowing={(e)=> e.cancel = true}
-                        onAppointmentFormOpening={e=>e.cancel=true}
-                        showAllDayPanel={false}
+                    appointmentRender={(e) => <CalendarDataCell data={e}/>}
+                    views={[{
+                        type: 'day', name: '3 Days', intervalCount: 3,
+                    }, 'week']}
+                    editing={{
+                        allowAdding: false,
+                        allowDeleting: false,
+                        allowUpdating: false,
+                        allowResizing: false,
+                        allowDragging: false,
+                        allowTimeZoneEditing: false,
+                    }}
+                    onAppointmentClick={(e) => e.cancel = true}
+                    onAppointmentDblClick={(e) => e.cancel = true}
+                    onCellClick={(e) => e.cancel = true}
+                    onAppointmentTooltipShowing={(e) => e.cancel = true}
+                    onAppointmentFormOpening={e => e.cancel = true}
+                    showAllDayPanel={false}
 
-                    >
+                >
 
-                    </Scheduler>
-                }
+                </Scheduler>
+                </Spin>
 
 
-                </div>
+            </div>
 
-        </div>
-    )
+        </div>)
 }
+
 export default DoctorReworkedCalendar;
--- src/Components/Dashboard/DoctorReworked/Fragments/DoctorReworkedCalendar/Fragments/CalendarDataCell.js
+++ src/Components/Dashboard/DoctorReworked/Fragments/DoctorReworkedCalendar/Fragments/CalendarDataCell.js
@@ -2,12 +2,26 @@ import React from "react";
 
 function CalendarDataCell({data}) {
     return(
-        <div>
-            <div>
-                {data.appointmentData.text}
+        <div className={'appointment-container'}>
+            <div className={'appointment-item'}>
+                {data.appointment?.icon&&<div className={'appointment-icon'}>
+                    <img src={data.appointment?.icon}/>
+                </div>}
+                <div className={'appointment-data'}>
+                    <div>
+                        {data.appointmentData.content}
+                    </div>
+                    <div>
+                        {data.appointmentData.booked_at.iso_string}
+                    </div>
+                </div>
             </div>
-            <div>
-                {data.appointmentData.booked_at.iso_string}
+
+            <div className={'road-trip'}>
+
+            </div>
+            <div className={'delete-icon'}>
+
             </div>
 
         </div>
