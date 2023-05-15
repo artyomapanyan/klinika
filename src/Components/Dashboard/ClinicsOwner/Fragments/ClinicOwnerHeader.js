2ed5c1b74e2d693d96931bef7b4bd424f5cbaa41
Tigran Ghumashyan <tigranghumashyan@gmail.com>
2023-05-15 18:44:30 +0400

    fixed styles

--- src/Components/Dashboard/ClinicsOwner/Fragments/ClinicOwnerHeader.js
+++ src/Components/Dashboard/ClinicsOwner/Fragments/ClinicOwnerHeader.js
@@ -1,106 +1,121 @@
-import React, {useEffect, useState} from 'react';
-import {Dropdown, Space} from "antd";
-import {t} from "i18next";
-import {postResource} from "../../../Functions/api_calls";
-import {useDispatch, useSelector} from "react-redux";
-import arrow_black from "../../../../dist/icons/arrow-black.svg";
-import dayjs from "dayjs";
-import Resources from "../../../../store/Resources";
+import React, { useEffect, useState } from 'react'
+import { Dropdown, Space } from 'antd'
+import { t } from 'i18next'
+import { postResource } from '../../../Functions/api_calls'
+import { useDispatch, useSelector } from 'react-redux'
+import arrow_black from '../../../../dist/icons/arrow-black.svg'
+import dayjs from 'dayjs'
+import Resources from '../../../../store/Resources'
 
-const currentMonth = new Date().getMonth();
-function ClinicOwnerHeader({noClinicSelect = false}) {
-    let token = useSelector((state) => state.auth.token);
-    const [items, setItems] = useState([]);
+const currentMonth = new Date().getMonth()
+function ClinicOwnerHeader({ noClinicSelect = false }) {
+	let token = useSelector(state => state.auth.token)
+	const [items, setItems] = useState([])
 
+	let dispatch = useDispatch()
+	let ownerClinics = useSelector(state => state?.owner)
 
+	useEffect(() => {
+		if (!noClinicSelect) {
+			postResource('ClinicOwnerClinics', 'list', token, '').then(response => {
+				if (response) {
+					setItems(
+						response.clinics.map((el, key) => {
+							if (key === 0) {
+								dispatch({
+									type: 'OWNER_DATA',
+									payload: {
+										id: el?.id,
+										month_key: currentMonth
+									}
+								})
+							}
+							return {
+								label: el?.name,
+								key: el?.id
+							}
+						})
+					)
+				}
+			})
+		}
+	}, [])
 
+	const onClick = ({ key }) => {
+		dispatch({
+			type: 'OWNER_DATA',
+			payload: {
+				id: key
+			}
+		})
+	}
 
+	const handleChange = ({ key }) => {
+		dispatch({
+			type: 'OWNER_DATA',
+			payload: {
+				month_key: key
+			}
+		})
+	}
 
-    let dispatch = useDispatch()
-    let ownerClinics = useSelector((state) => state?.owner);
-
-    useEffect(() => {
-        if(!noClinicSelect){
-            postResource('ClinicOwnerClinics','list', token,  '', ).then((response) => {
-                if(response) {
-                    setItems(response.clinics.map((el,key) => {
-                        if(key===0){
-                            dispatch({
-                                type:'OWNER_DATA',
-                                payload: {
-                                    id:  el?.id,
-                                    month_key:currentMonth
-                                }
-                            })
-                        }
-                        return{
-                            label: el?.name,
-                            key: el?.id
-                        }
-                    }))
-                }
-            })
-        }
-
-    }, [])
-
-
-    const onClick = ({key}) => {
-        dispatch({
-                type:'OWNER_DATA',
-                payload: {
-                    id: key
-                }
-            })
-
-    };
-
-    const handleChange = ({key}) => {
-            dispatch({
-                type:'OWNER_DATA',
-                payload: {
-                    month_key: key
-                }
-            })
-    }
-
-    const {Months} = Resources;
-    return(
-        <div className={'clinic_owner_header'}>
-            <div style={{margin:"40px 24px", fontSize:40, fontWeight:400}}>Dashboard</div>
-            <div>
-                <Dropdown
-                    menu={{
-                        items:Months,
-                        onClick:handleChange,
-                    }}
-                    trigger={['click']}
-                    className={'own_head_clinics'}
-                >
-                    <div className={'dashboard_head_dropdown'} style={{cursor:"pointer"}}>
-                        <div style={{ fontWeight: 400, fontSize:20}}>{Months.find(e=>e.key==ownerClinics.month_key)?.label??dayjs().format('MMM')}</div>
-                        <div style={{marginLeft: 20}} > <img alt={'arrow_black'} src={arrow_black}/></div>
-                    </div>
-
-                </Dropdown>
-            </div>
-            {!noClinicSelect&&<div>
-                <Dropdown
-                    menu={{
-                        items:items,
-                        onClick:onClick
-                    }}
-                    trigger={['click']}
-                    className={'own_head_clinics'}
-                >
-                    <div className={'dashboard_head_dropdown'} style={{cursor:"pointer"}}>
-                        <div style={{ fontWeight: 400, fontSize:20}}>{items.find((e)=>e.key==ownerClinics.id)?.label??t('All Clinics')}</div>
-                        <div style={{marginLeft: 20}}> <img alt={'arrow_black'} src={arrow_black}/></div>
-                    </div>
-
-                </Dropdown>
-            </div>}
-        </div>
-    )
+	const { Months } = Resources
+	return (
+		<div className={'clinic_owner_header'}>
+			<div style={{ margin: '40px 24px', fontSize: 40, fontWeight: 400 }}>
+				Dashboard
+			</div>
+			<div>
+				<Dropdown
+					menu={{
+						items: Months,
+						onClick: handleChange
+					}}
+					trigger={['click']}
+					className={'own_head_clinics'}
+				>
+					<div
+						className={'dashboard_head_dropdown'}
+						style={{ cursor: 'pointer' }}
+					>
+						<div style={{ fontWeight: 400, fontSize: 20 }}>
+							{Months.find(e => e.key == ownerClinics.month_key)?.label ??
+								dayjs().format('MMM')}
+						</div>
+						<div style={{ marginLeft: 20 }}>
+							{' '}
+							<img alt={'arrow_black'} src={arrow_black} />
+						</div>
+					</div>
+				</Dropdown>
+			</div>
+			{!noClinicSelect && (
+				<div>
+					<Dropdown
+						menu={{
+							items: items,
+							onClick: onClick
+						}}
+						trigger={['click']}
+						className={'own_head_clinics'}
+					>
+						<div
+							className={'dashboard_head_dropdown'}
+							style={{ cursor: 'pointer' }}
+						>
+							<div style={{ fontWeight: 400, fontSize: 20 }}>
+								{items.find(e => e.key == ownerClinics.id)?.label ??
+									t('All Clinics')}
+							</div>
+							<div style={{ marginLeft: 20 }}>
+								{' '}
+								<img alt={'arrow_black'} src={arrow_black} />
+							</div>
+						</div>
+					</Dropdown>
+				</div>
+			)}
+		</div>
+	)
 }
-export default ClinicOwnerHeader;
+export default ClinicOwnerHeader
