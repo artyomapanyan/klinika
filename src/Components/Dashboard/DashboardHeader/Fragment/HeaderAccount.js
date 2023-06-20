54ce7db34b8c359240ce1afeca0fd3c711fd8883
Артём <artem.a@cmartech.sa>
2023-06-20 17:40:54 +0400

    bagi

--- src/Components/Dashboard/DashboardHeader/Fragment/HeaderAccount.js
+++ src/Components/Dashboard/DashboardHeader/Fragment/HeaderAccount.js
@@ -46,12 +46,14 @@ function HeaderAccount() {
 	}, [role, elem])
 
 	useEffect(() => {
-
+		setInterval(() => {
 			setLoading(true)
 			postResource('Notifications', 'UnreadLastNotification', token, ``).then(response => {
 				setNotifications(response)
 				setLoading(false)
 			})
+		}, 60000)
+
 
 
 	}, [role])
