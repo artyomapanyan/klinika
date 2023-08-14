0afcec689a4bb339742743352013a9521803e822
Артём <artem.a@cmartech.sa>
2023-08-14 19:03:32 +0400

    super

--- src/Components/Dashboard/DashboardMenu/Menulist.js
+++ src/Components/Dashboard/DashboardMenu/Menulist.js
@@ -340,6 +340,196 @@ export default {
 
 
     ],
+    'super-admin': [
+        {
+            key: 'super-admin',
+            icon: <img alt={'icons'} src={dash1}/>,
+            label: 'Dashboard',
+        },
+        {
+            label: t(`Inputs`),
+            icon: <ProfileOutlined style={{color:'#ce4e99', fontSize:20}} />,
+            key:'inputs',
+            children: [
+                {
+                    key: 'countries',
+                    label: t(`Countries`),
+                    permission:'Country'
+                },
+                {
+                    key: 'regions',
+                    label: t(`Areas`),
+                    permission:'Region'
+                },
+                {
+                    key: 'cities',
+                    label: t(`Cities`),
+                    permission:'City'
+                },
+                {
+                    key: 'categories',
+                    label: t(`Categories`),
+                    permission:'Category'
+                },
+                {
+                    key: 'sub-categories',
+                    label: t(`Sub categories`),
+                    permission:'SubCategory'
+                },
+                {
+                    key: 'services',
+                    label: t(`Services`),
+                    permission:'Service'
+                },
+                {
+                    key: 'sub-services',
+                    label: t(`Sub services`),
+                    permission:'SubService'
+                },
+                {
+                    key: 'nursing-tasks',
+                    label: t(`Nursing tasks`),
+                    permission:'NursingTask'
+                },
+                {
+                    key: 'insurance-companies',
+                    label: t(`Insurance companies`),
+                    permission:'InsuranceCompany'
+                },
+                {
+                    key: 'bug-reports',
+                    label: t(`Bug Report Topics`),
+                    permission:'Taxonomy'
+                },
+                {
+                    key: 'reports',
+                    label: t(`Reports`),
+                    permission:'Report'
+                },
+                {
+                    key: 'reports/new',
+                    label: t(`Requests`),
+                    permission:'Report'
+                },
+                {
+                    key: 'payment-methods',
+                    label: t(`Payment Methods`),
+                    permission:'PaymentMethod'
+                },
+                {
+                    key: 'specialties',
+                    label: t(`Specialties`),
+                    permission:'Taxonomy'
+                },
+                {
+                    key: 'sub-specialties',
+                    label: t(`Sub Specialties`),
+                    permission:'Taxonomy'
+                },
+                {
+                    key: 'posts',
+                    label: t(`Posts`),
+                    permission:'Post'
+                },
+                {
+                    key: 'translations',
+                    label: t(`Translations`),
+                    permission:'Translation'
+                },
+                {
+                    key: 'preferences',
+                    label: t(`Preferences`),
+                    permission:'Preference'
+                },
+                {
+                    key: 'taxonomies',
+                    label: t(`Laboratory`),
+                    permission:'Taxonomy'
+                },
+            ]
+        },
+        {
+            label: t(`User`),
+            icon: <img alt={'icons'} src={dash4}/>,
+            children: [
+                {
+                    key: 'roles',
+                    label: t(`Roles`),
+                    permission:'Role'
+                },
+                {
+                    key: 'users',
+                    label: t(`Users`),
+                    permission:'User'
+                },
+            ]
+        },
+        {
+            label: t(`Accounts`),
+            icon: <img alt={'icons'} src={dash2}/>,
+            children: [
+                {
+                    key: 'doctors',
+                    label: t(`Doctors`),
+                    permission:'Doctor'
+                },
+                {
+                    key: 'clinics',
+                    label: t(`Clinics`),
+                    permission:'Clinic'
+                },
+            ]
+        },
+
+        {
+            label: t(`Invoices`),
+            icon: <DollarOutlined style={{color:'#ce4e99', fontSize:20}}  />,
+            children: [
+                {
+                    icon:<DollarOutlined style={{color:'#c98a1e', fontSize:20}}/>,
+                    key: 'invoice-items',
+                    label:  t(`Invoice Items`),
+                    permission:'InvoiceItem'
+                },
+                {
+                    icon:<DollarOutlined style={{color:'#c98a1e', fontSize:20}}/>,
+                    key: 'invoices',
+                    label:  t(`Invoices`),
+                    permission:'Invoice'
+                },
+            ]
+        },
+        {
+            label: t(`Marketing`),
+            icon: <NotificationOutlined style={{color:'#ce4e99', fontSize:20}}  />,
+            children: [
+                {
+                    icon:<PercentageOutlined style={{color:'#c98a1e', fontSize:20}} />,
+                    key: 'offers',
+                    label:  t(`Offers`),
+                    permission:'Offer'
+                },
+                {
+                    icon:<RedEnvelopeOutlined style={{color:'#c98a1e', fontSize:20}}/>,
+                    key: 'coupons',
+                    label:  t(`Coupons`),
+                    permission:'Coupon'
+                },
+            ]
+        },
+        {
+            key: 'appointments',
+            icon: <DiffOutlined style={{color:'#ce4e99', fontSize:20}} />,
+            label: t(`Appointments`),
+        },
+        {
+            key: 'notifications',
+            icon: <BellOutlined style={{color:'#ce4e99', fontSize:20}} />,
+            label: t(`Notifications`),
+        },
+
+
+    ],
 
     'admin': [
         {
