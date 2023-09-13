10da8236719bf2731d434db32d31ad95c630d017
Артём <artem.a@cmartech.sa>
2023-09-13 18:08:03 +0400

    mobile design doctor image main

--- src/Components/Dashboard/DoctorReworked/Fragments/DoctorReworkedCalendar/Fragments/DateTimeSelect.js
+++ src/Components/Dashboard/DoctorReworked/Fragments/DoctorReworkedCalendar/Fragments/DateTimeSelect.js
@@ -90,7 +90,7 @@ function DateTimeSelect({setBookedAtState, formState, bookedAtState, date, setDa
                     }
                 })
                 setDisabledDays(day)
-                //console.log(responses, day, 'hhhh')
+
 
             })
         }
