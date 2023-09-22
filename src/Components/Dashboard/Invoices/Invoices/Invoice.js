5397707644bf9fbc04214cd498d9050c6bc17b18
Артём <artem.a@cmartech.sa>
2023-09-22 17:48:14 +0400

    merge offer_ar new

--- src/Components/Dashboard/Invoices/Invoices/Invoice.js
+++ src/Components/Dashboard/Invoices/Invoices/Invoice.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, { useRef, useState} from "react";
 import {useNavigate, useParams} from "react-router";
 import {useDispatch, useSelector} from "react-redux";
 import {createResource, postResource, updateResource, useGetResourceSingle} from "../../../Functions/api_calls";
