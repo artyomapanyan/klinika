88fc3dac954ea656733067b100f9528e52301e54
Артём <artem.a@cmartech.sa>
2023-10-06 15:06:54 +0400

    clinic service2

--- src/Components/Dashboard/Clinics/Fragments/ClinicTabEssentials.js
+++ src/Components/Dashboard/Clinics/Fragments/ClinicTabEssentials.js
@@ -344,7 +344,10 @@ function ClinicTabEssentials({loadingState, dataState,addDataState}) {
         values.has_physical_therapy_home_visit_service = values.has_physical_therapy_home_visit_service === true
         values.has_physical_therapy_clinic_visit_service = values.has_physical_therapy_clinic_visit_service === true
 
-        values.enable_telehealth_service ? values.enable_telehealth_service = true : values.enable_telehealth_service = 0
+        //values.enable_telehealth_service ? values.enable_telehealth_service = true : values.enable_telehealth_service = 0
+        values.enable_telehealth_service = !!values.enable_telehealth_service
+        console.log(values.enable_telehealth_service, 'aaa')
+
         values.enable_home_visit_service ? values.enable_home_visit_service = true : values.enable_home_visit_service = 0
         values.enable_clinic_visit_service ? values.enable_clinic_visit_service = true : values.enable_clinic_visit_service = 0
         values.enable_laboratory_home_visit_service ? values.enable_laboratory_home_visit_service = true : values.enable_laboratory_home_visit_service = 0
