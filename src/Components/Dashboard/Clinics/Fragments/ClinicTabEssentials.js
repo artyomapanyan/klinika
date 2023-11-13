f3eba8137a37fc4770cb5e5afce10c1e1cee2d63
Артём <artem.a@cmartech.sa>
2023-11-13 17:12:18 +0400

    clinic durations

--- src/Components/Dashboard/Clinics/Fragments/ClinicTabEssentials.js
+++ src/Components/Dashboard/Clinics/Fragments/ClinicTabEssentials.js
@@ -408,14 +408,14 @@ function ClinicTabEssentials({loadingState, dataState,addDataState}) {
             ...values
         }))
 
-        if(values?.service_settings?.clinic_visit?.duration) {
+
+
+        if(values?.service_settings?.clinic_visit?.duration && values?.service_settings?.laboratory_clinic_visit?.duration) {
             values.service_settings.laboratory_clinic_visit.duration = values?.service_settings?.clinic_visit?.duration
 
-        }if(values?.service_settings?.home_visit?.duration) {
+        }if(values?.service_settings?.home_visit?.duration && values?.service_settings?.laboratory_home_visit?.duration) {
             values.service_settings.laboratory_home_visit.duration = values?.service_settings?.home_visit?.duration
         }
-
-
 
 
 
