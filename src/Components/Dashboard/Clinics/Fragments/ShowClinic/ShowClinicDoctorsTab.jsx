7d8fea1d68cb44c6435b3388688cc2d63791b3e3
Артём <artem.a@cmartech.sa>
2023-12-20 14:42:25 +0400

    clinic show doctors

--- src/Components/Dashboard/Clinics/Fragments/ShowClinic/ShowClinicDoctorsTab.jsx
+++ src/Components/Dashboard/Clinics/Fragments/ShowClinic/ShowClinicDoctorsTab.jsx
@@ -10,41 +10,22 @@ import {useSelector} from "react-redux";
 import Preloader from "../../../../Preloader";
 
 const ShowClinicDoctorsTab = ({dataState}) => {
-    let token = useSelector((state) => state.auth.token);
 
-    const [loading, setLoading] = useState(false);
-    const [data, setData] = useState([]);
-
-
-
-    useEffect(() => {
-        setLoading(true)
-        postResource('ClinicDoctor', 'list', token, '', {
-            clinic:dataState?.data?.id,
-
-        }).then((response) => {
-
-            setData(response.items?.map((el) => {
-               return {
-                   key: el.id,
-                   doctor: <div key={el?.id} style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-                           <div>
-                               <Avatar size={64} icon={<UserOutlined />} />
-                           </div>
-                           <div  style={{padding:10, minWidth: 150}}>{el.doctor.first} {el.doctor.last}</div>
-                       </div>,
-                   specialty: <div>{el?.specialties?.map((specialty, key) => {
-                           return<span key={key}><span style={{backgroundColor: '#d24a9a', color: '#ffffff', marginLeft: 5, padding: 1, borderRadius: 2}}>{specialty?.title}</span></span>
-                       })}
-                       </div>
-               }
-            }))
-            setLoading(false)
-        })
-    }, [])
-
-
-
+    let dataDoctors = dataState?.data?.doctors?.map((el) => {
+        return {
+            key: el.id,
+            doctor: <div key={el?.id} style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                <div>
+                    <Avatar size={64} icon={<UserOutlined />} />
+                </div>
+                <div  style={{padding:10, minWidth: 150}}>{el?.first} {el?.last}</div>
+            </div>,
+            specialty: <div>{el?.specialties?.map((specialty, key) => {
+                return<span key={key}><span style={{backgroundColor: '#d24a9a', color: '#ffffff', marginLeft: 5, padding: 1, borderRadius: 2}}>{specialty?.title}</span></span>
+            })}
+            </div>
+        }
+    })
 
 
     const columns = [
@@ -63,9 +44,8 @@ const ShowClinicDoctorsTab = ({dataState}) => {
 
   return (
     <div style={{width: '100%'}}>
-        {
-            loading ? <Preloader /> : <Table rowKey={data?.key} dataSource={data} columns={columns} style={{width: '100%'}} pagination={false}/>
-        }
+        <Table rowKey={dataState?.data?.doctors?.key} dataSource={dataDoctors} columns={columns} style={{width: '100%'}} pagination={false}/>
+
 
 
     </div>
