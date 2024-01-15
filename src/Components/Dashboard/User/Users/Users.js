719a3db85e5498c59398643fb295f66df534c18f
Ibra Sabry <ibrahim.s@smartech.sa>
2024-01-15 16:25:12 +0200

    Users list - Should be added role column ticket no. 86bx2d560

--- src/Components/Dashboard/User/Users/Users.js
+++ src/Components/Dashboard/User/Users/Users.js
@@ -4,7 +4,10 @@ import TableFilterElement from "../../../Fragments/TableFilterElements/TableFilt
 import {t} from "i18next";
 import DateParser from "../../../Fragments/DateParser";
 import PermCheck from "../../../Fragments/PermCheck";
+import {useSelector} from "react-redux";
+
 function Users() {
+    let language = useSelector((state) => state.app.current_locale)
     return(
         <div>
             <ResourceTable resource={'User'}
@@ -44,6 +47,17 @@ function Users() {
                     dataIndex:'clinic_names',
                     key:'clinic_names',
                     className: 'table_clinics_column'
+                },
+                {
+                    title:t('Roles'),
+                    dataIndex:'roles',
+                    key:'roles',
+                    translatable:true,
+                    render:(e, record) => {
+                        return record?.roles?.map((e, i) => {
+                            return e?.name[language] + (i === record?.roles.length - 1 ? '' : ', ')
+                        })
+                    }
                 },
                 {
                     dataIndex:['updated_at','iso_string'],
