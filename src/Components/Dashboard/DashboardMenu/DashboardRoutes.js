ec52398ababeda9c322efbee022c13d8a19df5f8
Артём <artem.a@cmartech.sa>
2024-01-19 19:35:31 +0400

    merge all tasks1

--- src/Components/Dashboard/DashboardMenu/DashboardRoutes.js
+++ src/Components/Dashboard/DashboardMenu/DashboardRoutes.js
@@ -295,7 +295,7 @@ function DashboardRoutes(){
         <Route path={'clinics/doctors/:id'} element={<ClinicDoctorUpdate />}/>
         <Route path={'Reports/new'} element={<Report />}/>
 
-        <Route path={'appointments/:id/doctor'} element={<AuthCheck permission={'Patient:viewPatientCard '}> <Patient /></AuthCheck>}/>
+        {/*<Route path={'appointments/:id/doctor'} element={<AuthCheck permission={'Patient:viewPatientCard '}> <Patient /></AuthCheck>}/>*/}
 
         <Route path={'appointments/:id/doctor'} element={<AuthCheck roleKey={'doctor'} ><Patient/></AuthCheck>}/>
 
