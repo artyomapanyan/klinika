3181a87059ad278fa5721c6d92662631ba73a92e
Ibra Sabry <ibrahim.s@smartech.sa>
2024-03-07 00:30:50 +0200

    handle create appointment from patient list page

--- src/Components/Dashboard/Appointments/Appointment.js
+++ src/Components/Dashboard/Appointments/Appointment.js
@@ -1,7 +1,11 @@
-import { useNavigate } from 'react-router'
+import { useNavigate, useParams } from 'react-router'
 import React, { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { postResource, createResource } from '../../Functions/api_calls'
+import {
+	postResource,
+	createResource,
+	useGetResourceSingle
+} from '../../Functions/api_calls'
 import { Button, Form, Space, Row, Col, Avatar } from 'antd'
 import { t } from 'i18next'
 import Preloader from '../../Preloader'
@@ -15,11 +19,13 @@ import AppointmentCalendar from './Fragments/AppointmentCalendar/AppointmentCale
 import { UserOutlined } from '@ant-design/icons'
 import dayjs from 'dayjs'
 import './Appointment.sass'
-import {notification} from "antd";
+import { notification } from 'antd'
 
 const resource = 'Appointment'
 
 function Appointment() {
+	const params = useParams()
+
 	let token = useSelector(state => state.auth.token)
 	let role = useSelector(state => state.auth.selected_role?.key)
 	let doctor_id = useSelector(state => state.auth?.user?.id)
@@ -47,6 +53,26 @@ function Appointment() {
 	const patientFormRef = useRef()
 	const appointmentFormRef = useRef()
 	const fetchedUsers = useRef([])
+
+	//Get the patient from the route, if navigating from patient ;list page
+	useGetResourceSingle(
+		'Patient',
+		params.id,
+		{},
+		params.id
+			? response => {
+					setCodeAndPhone(prevState => ({
+						...prevState,
+						phone_country_code: response?.phone_country_code,
+						phone_number: response?.phone_number
+					}))
+					searchFormRef.current.setFieldsValue({
+						patient: '+' + response.phone_country_code + response.phone_number
+					})
+					return data
+			  }
+			: null
+	)
 
 	//assign user clinic and doctor id to the appointment object
 	useEffect(() => {
@@ -140,6 +166,21 @@ function Appointment() {
 		return [name, item]
 	}
 
+	const isPatientAuth = clinic_id => {
+		postResource('PatientSearch', 'single', token, '', {
+			...codeAndPhone,
+			clinic_id: clinic_id
+		}).then(response => {
+			if (response.message) {
+				setPageState('unauthorized')
+				setPatient(null)
+			} else {
+				setPageState('retrieved')
+				setPatient(response.items[0])
+			}
+		})
+	}
+
 	const sendCode = () => {
 		setSendCodeLoading(true)
 		postResource(
@@ -163,7 +204,7 @@ function Appointment() {
 				clinic_id: data?.clinic_id,
 				code: data?.code
 			}).then(response => {
-				if(response?.patient){
+				if (response?.patient) {
 					fetchedUsers.current.push(response?.patient)
 					setData(prevState => ({
 						...prevState,
@@ -180,69 +221,111 @@ function Appointment() {
 	useEffect(() => {
 		if (data?.clinic_id) {
 			setServiceTypeState([])
-			postResource('Clinic', 'single', token, data?.clinic_id).then(
-				responses => {
-					setServiceTypeState(
-						[
-							{
-								service: responses?.has_telehealth_service,
-								id: 'telehealth',
-								name: 'Telehealth'
-							},
-							{
-								service: responses?.has_clinic_visit_service,
-								id: 'clinic_visit',
-								name: 'Clinic Visit'
-							},
-							{
-								service: responses?.has_home_visit_service,
-								id: 'home_visit',
-								name: 'Home Visit'
-							},
-							{
-								service: responses?.has_physical_therapy_home_visit_service,
-								id: 'physical_therapy_home_visit',
-								name: 'Physical Therapy Home Visit'
-							},
-							{
-								service: responses?.has_physical_therapy_clinic_visit_service,
-								id: 'physical_therapy_clinic_visit',
-								name: 'Physical Therapy Clinic Visit'
-							},
-							{
-								service: responses?.has_laboratory_home_visit_service,
-								id: 'laboratory_home_visit',
-								name: 'Laboratory Home Visit'
-							},
-							{
-								service: responses?.has_nursing_service,
-								id: 'nursing',
-								name: 'Nursing'
-							},
-							{
-								service: responses?.has_laboratory_clinic_visit_service,
-								id: 'laboratory_clinic_visit',
-								name: 'Laboratory Clinic Visit'
-							}
-						].filter(el => el.service === true)
-					)
-				}
-			)
+			if (role === 'doctor') {
+				Promise.all([
+					postResource(
+						'clinicDoctorBydoctorAndClinic',
+						'single',
+						token,
+						`${doctor_id}/${data?.clinic_id}`
+					),
+					postResource('Clinic', 'single', token, data?.clinic_id)
+				]).then(responses => {
+					let doctorActiveServices = Object.keys(
+						responses[0]?.activated_services
+					)?.map(el => {
+						return {
+							service: true,
+							id: el,
+							name: el.replaceAll('_', ' ')
+						}
+					})
+					let clinicActiveServices = [
+						{
+							service: responses[1]?.has_laboratory_home_visit_service,
+							id: 'laboratory_home_visit',
+							name: 'Laboratory Home Visit'
+						},
+						{
+							service: responses[1]?.has_nursing_service,
+							id: 'nursing',
+							name: 'Nursing'
+						},
+						{
+							service: responses[1]?.has_laboratory_clinic_visit_service,
+							id: 'laboratory_clinic_visit',
+							name: 'Laboratory Clinic Visit'
+						}
+					].filter(el => el.service === true)
+
+					setServiceTypeState([
+						...doctorActiveServices,
+						...clinicActiveServices
+					])
+				})
+			} else {
+				postResource('Clinic', 'single', token, data?.clinic_id).then(
+					responses => {
+						setServiceTypeState(
+							[
+								{
+									service: responses?.has_telehealth_service,
+									id: 'telehealth',
+									name: 'Telehealth'
+								},
+								{
+									service: responses?.has_clinic_visit_service,
+									id: 'clinic_visit',
+									name: 'Clinic Visit'
+								},
+								{
+									service: responses?.has_home_visit_service,
+									id: 'home_visit',
+									name: 'Home Visit'
+								},
+								{
+									service: responses?.has_physical_therapy_home_visit_service,
+									id: 'physical_therapy_home_visit',
+									name: 'Physical Therapy Home Visit'
+								},
+								{
+									service: responses?.has_physical_therapy_clinic_visit_service,
+									id: 'physical_therapy_clinic_visit',
+									name: 'Physical Therapy Clinic Visit'
+								},
+								{
+									service: responses?.has_laboratory_home_visit_service,
+									id: 'laboratory_home_visit',
+									name: 'Laboratory Home Visit'
+								},
+								{
+									service: responses?.has_nursing_service,
+									id: 'nursing',
+									name: 'Nursing'
+								},
+								{
+									service: responses?.has_laboratory_clinic_visit_service,
+									id: 'laboratory_clinic_visit',
+									name: 'Laboratory Clinic Visit'
+								}
+							].filter(el => el.service === true)
+						)
+					}
+				)
+			}
 		}
 	}, [data?.clinic_id])
 
 	//Calculate the invoice price
 	useEffect(() => {
-		if (
-			data?.booked_at &&
-			((pageState === 'creation' && nationality) || data.patient_id)
-		) {
+		if (data?.booked_at) {
 			let appointment = { ...data }
 
 			if (!appointment?.patient_id) {
 				appointment.patient = { country_id: nationality }
 				delete appointment.patient_id
 			}
+			setInvoicePrice(0)
 			postResource(resource, 'InvoicePrice', token, '', appointment).then(
 				response => {
 					setInvoicePrice(response.total_price ?? 0)
@@ -292,7 +375,7 @@ function Appointment() {
 				.catch(error => {
 					console.error('Validation failed:', error)
 				})
-		} else {
+		} else if (pageState === 'selected') {
 			delete appointment.patient
 			searchFormRef.current
 				.validateFields()
@@ -302,6 +385,15 @@ function Appointment() {
 				.catch(error => {
 					console.error('Validation failed:', error)
 				})
+		} else if (pageState === 'retrieved') {
+			appointment.patient_id = params.id
+			createAppointment(appointment)
+		} else if (pageState === 'unauthorized' || pageState === 'codeSent') {
+			notification.error({
+				message: 'Error',
+				description: t('Please verify patient for the selected clinic'),
+				placement: 'bottomRight'
+			})
 		}
 	}
 
@@ -343,12 +435,13 @@ function Appointment() {
 						? data?.lab_tests.filter((_, index) => index !== idx)
 						: data?.lab_tests
 			}))
-		}
-		else{
+		} else {
 			notification.error({
-				message: "Error",
-				description: t('The appointment must have at least one lab test or one lab package'),
-				placement: "bottomRight"
+				message: 'Error',
+				description: t(
+					'The appointment must have at least one lab test or one lab package'
+				),
+				placement: 'bottomRight'
 			})
 		}
 	}
@@ -357,15 +450,16 @@ function Appointment() {
 		if (data?.nursing_tasks.length > 1) {
 			setData(prevState => ({
 				...prevState,
-				nursingTasksArray: data?.nursingTasksArray.filter((_, index) => index !== idx),
+				nursingTasksArray: data?.nursingTasksArray.filter(
+					(_, index) => index !== idx
+				),
 				nursing_tasks: data?.nursing_tasks.filter((_, index) => index !== idx)
 			}))
-		}
-		else{
+		} else {
 			notification.error({
-				message: "Error",
+				message: 'Error',
 				description: t('The appointment must have at least one nursing task'),
-				placement: "bottomRight"
+				placement: 'bottomRight'
 			})
 		}
 	}
@@ -421,6 +515,9 @@ function Appointment() {
 											searchFormRef?.current?.setFieldsValue({
 												patient_id: undefined
 											})
+											if (params?.id) {
+												isPatientAuth(e)
+											}
 										}
 									}}
 									resourceParams={{
@@ -430,84 +527,100 @@ function Appointment() {
 								/>
 							</Col>
 						) : null}
-						<Col lg={4} className='gutter-row'>
-							<FormInput
-								label={t('Country Code')}
-								name={'phone_country_code'}
-								inputType={'resourceSelect'}
-								rules={[{ required: true }]}
-								initialValue={
-									data?.appointment?.patient?.phone_country_code
-										? data?.appointment?.patient?.phone_country_code
-										: `(966) ${
-												language === 'ar'
-													? 'المملكة العربية السعودية'
-													: 'Saudi Arabia'
-										  }`
-								}
-								handleMapItems={handleMapItems}
-								customSearchKey={'phone_code'}
-								inputProps={{
-									onChange: e =>
-										setCodeAndPhone(prevState => ({
-											...prevState,
-											phone_country_code: e
-										}))
-								}}
-								disabled={!data?.clinic_id}
-								resource={'Country'}
-							/>
-						</Col>
-						<Col lg={role === 'clinic-manager' ? 14 : 8} className='gutter-row'>
-							<FormInput
-								label={t('Select Patient (Search By phone number)')}
-								name={'patient_id'}
-								suffixIcon={<img src={user_icon} alt={'user_icon'} />}
-								inputType={'resourceSelect'}
-								rules={[{ required: pageState !== 'creation' }]}
-								searchConfigs={{ minLength: 6 }}
-								inputProps={{
-									onSearch: e => {
-										if (e)
-											setCodeAndPhone(prevState => ({
-												...prevState,
-												phone_number: e
-											}))
-									},
-									notFoundContent: (
-										<div
-											style={{
-												display: 'flex',
-												flexDirection: 'row',
-												justifyContent: 'space-between'
-											}}
-										>
-											<div>{t('Not found')}</div>
-										</div>
-									)
-								}}
-								resourceParams={{
-									phone_country_code: data?.phone_country_code
-										? data?.phone_country_code?.length > 3
-											? data?.phone_country_code?.slice(
-													data?.phone_country_code?.indexOf('(') + 1,
-													data?.phone_country_code?.indexOf(')')
-											  )
-											: data?.phone_country_code
-										: '966',
-									clinic_id: data?.clinic_id
-								}}
-								handleMapItems={(item, name, status) =>
-									searchByNumber(item, name, status)
-								}
-								handleStatus={true}
-								customSearchKey={'phone_number'}
-								disabled={!data?.clinic_id}
-								resource={'PatientSearch'}
-							/>
-						</Col>
+						{params.id ? (
+							<Col lg={12} className='gutter-row'>
+								<FormInput
+									label={t('Patient')}
+									name={'patient'}
+									inputDisabled={true}
+								/>
+							</Col>
+						) : (
+							<>
+								<Col lg={4} className='gutter-row'>
+									<FormInput
+										label={t('Country Code')}
+										name={'phone_country_code'}
+										inputType={'resourceSelect'}
+										rules={[{ required: true }]}
+										initialValue={
+											patient?.phone_country_code
+												? patient?.phone_country_code
+												: `(966) ${
+														language === 'ar'
+															? 'المملكة العربية السعودية'
+															: 'Saudi Arabia'
+												  }`
+										}
+										handleMapItems={handleMapItems}
+										customSearchKey={'phone_code'}
+										inputProps={{
+											onChange: e =>
+												setCodeAndPhone(prevState => ({
+													...prevState,
+													phone_country_code: e
+												}))
+										}}
+										disabled={!data?.clinic_id}
+										resource={'Country'}
+									/>
+								</Col>
+								<Col
+									lg={role === 'clinic-manager' ? 14 : 8}
+									className='gutter-row'
+								>
+									<FormInput
+										label={t('Select Patient (Search By phone number)')}
+										name={'patient_id'}
+										suffixIcon={<img src={user_icon} alt={'user_icon'} />}
+										inputType={'resourceSelect'}
+										rules={[{ required: pageState !== 'creation' }]}
+										searchConfigs={{ minLength: 6 }}
+										inputProps={{
+											onSearch: e => {
+												if (e)
+													setCodeAndPhone(prevState => ({
+														...prevState,
+														phone_number: e
+													}))
+											},
+											notFoundContent: (
+												<div
+													style={{
+														display: 'flex',
+														flexDirection: 'row',
+														justifyContent: 'space-between'
+													}}
+												>
+													<div>{t('Not found')}</div>
+												</div>
+											)
+										}}
+										resourceParams={{
+											phone_country_code: data?.phone_country_code
+												? data?.phone_country_code?.length > 3
+													? data?.phone_country_code?.slice(
+															data?.phone_country_code?.indexOf('(') + 1,
+															data?.phone_country_code?.indexOf(')')
+													  )
+													: data?.phone_country_code
+												: '966',
+											clinic_id: data?.clinic_id
+										}}
+										handleMapItems={(item, name, status) =>
+											searchByNumber(item, name, status)
+										}
+										handleStatus={true}
+										customSearchKey={'phone_number'}
+										disabled={!data?.clinic_id}
+										resource={'PatientSearch'}
+									/>
+								</Col>
+							</>
+						)}
 						<Col lg={6} className='gutter-row'>
-							{pageState === 'initial' || pageState === 'creation' ? (
+							{(pageState === 'initial' || pageState === 'creation') &&
+							!params.id ? (
 								<Button
 									onClick={() => {
 										setData(prevState => ({
@@ -599,7 +712,7 @@ function Appointment() {
 					setNationality={setNationality}
 				></CreatePatient>
 			) : null}
-			{pageState === 'selected' ? (
+			{pageState === 'selected' || pageState === 'retrieved' ? (
 				<div>
 					<CreatePatient
 						data={patient}
@@ -663,6 +776,7 @@ function Appointment() {
 													resourceParams={{
 														type: Resources.TaxonomyTypes.SPECIALTY,
 														has_parent: 0,
+														has_doctor: 1,
 														clinic: data?.clinic_id,
 														doctor: role === 'doctor' ? doctor_id : undefined
 													}}
@@ -740,16 +854,18 @@ function Appointment() {
 										</Col>
 									) : null}
 									<Col lg={5} className='gutter-row'>
-										<Space>
-											<div>
-												<div
-													className={'cl_manager_modal_dr_name'}
-													style={{ fontWeight: 400 }}
-												>
-													{invoicePrice} SAR
+										{invoicePrice ? (
+											<Space>
+												<div>
+													<div
+														className={'cl_manager_modal_dr_name'}
+														style={{ fontWeight: 400 }}
+													>
+														{invoicePrice} SAR
+													</div>
 												</div>
-											</div>
-										</Space>
+											</Space>
+										) : null}
 									</Col>
 									<Col lg={6} className='gutter-row'>
 										<Space>
