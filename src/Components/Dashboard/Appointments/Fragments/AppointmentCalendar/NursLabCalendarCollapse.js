d2cca996c60be2b2895b53ced86a8d360faeba68
Hayk <hayk.b@smartech.com>
2024-04-16 15:04:34 +0400

    Radiology service_ appointment booking and remove service fee

--- src/Components/Dashboard/Appointments/Fragments/AppointmentCalendar/NursLabCalendarCollapse.js
+++ src/Components/Dashboard/Appointments/Fragments/AppointmentCalendar/NursLabCalendarCollapse.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Modal } from 'antd'
 import AppointmentCalendarModal from './AppointmentCalendarModal'
 import gray_grid from '../../../../../dist/icons/bg_pattern.jpg'
